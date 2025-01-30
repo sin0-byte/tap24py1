85f70a6942fb2f259e1365e77971529a3a34339e
sin0-byte <tahsinparvez19@icloud.com>
2025-01-30 19:07:54 +0000

    begin

---
+++ main.py
@@ -0,0 +1,24 @@
+# python 1
+# basic data types
+
+h = "hello"
+w = "world"
+
+print(h,w)
+print(h+w)
+
+n1 = '5'
+print(h+n1)
+
+# how to concatenate str with number?
+# typecasting
+
+print(h+str(n1))
+
+multilinestring = """
+    str() - cast to string
+    int() - cast to integer
+    float() - cast to float
+"""
+
+print(h+str(n1))
