8fdfffcb4279c1f59fd08425bc00e6412ccdd453
sin0-byte <tahsinparvez19@icloud.com>
2025-01-30 14:32:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,36 @@ multilinestring = """
 """
 
 print(h+str(n1))
+
+# formatted strings
+n1 = 5 
+n2 = 3
+res = n1 +n2
+out = f"{n1}+{n2}={n1+n2}"
+print(out)
+
+# logical operators
+# code block
+
+n1 = 5
+n2 = 3
+
+if n1<n2:
+
+    print("lesser")
+    print("<"*6)
+
+elif n1==n2:
+    print("equal")
+    print('====') 
+
+else: 
+    print("greater")
+    print(">"*len("greater"))
+
+
+# blocks in python use indentation
+# used in if statements/function def
+
+f = 17.4
+print(type(f))
